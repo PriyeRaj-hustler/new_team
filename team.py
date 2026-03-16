eb0385392d409d0457b48ce88dc4ac970971592a
PriyeRaj-hustler <as-mac-1259@AS-MAC-1259s-MacBook-Air.local>
2026-03-16 18:55:55 +0530

    added team.py in feature-update

--- team.py
+++ team.py
@@ -1,5 +1,7 @@
 def greet():
     print("Hi first greeting for you")
+    print("Hi i'm from feature-update")
 
 def farewell():
     print("Hi first farewell for you")
+    print("Hi i'm from farewell")
